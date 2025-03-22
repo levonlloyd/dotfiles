b9932ddd9dfdb2e0c09930f67b8749cfeda969ac
levon <levon@Levons-Mac-mini.local>
2025-03-22 14:06:01 -0400

    Add ghostty config and refactor so that individual app configs can be stowed individually.

--- .config/nvim/lua/config/keymaps.lua
+++
@@ -1,8 +0,0 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
---
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
--- .config/nvim/lua/plugins/oil.lua
+++
@@ -1,7 +0,0 @@
-vim.keymap.set("n", "-", require("oil").open, { desc = "Open parent directory" })
-return {
-  "stevearc/oil.nvim",
-  opts = {},
-  -- Optional dependencies
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-}
